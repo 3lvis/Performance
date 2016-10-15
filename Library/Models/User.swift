e163bd313b4962ab26b0a886e605e582115045bf
Artur <artu-antonov@yandex.ru>
2016-10-15 12:43:22 +0300

    Updated to swift 3

--- Library/Models/User.swift
+++ Library/Models/User.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 struct User {
-    static func light() -> [[String : AnyObject]] {
-        var users = [[String : AnyObject]]()
+    static func light() -> [[String : Any]] {
+        var users = [[String : Any]]()
         for _ in 0..<500 {
             users.append(self.generate())
         }
@@ -10,15 +10,15 @@ struct User {
         return users
     }
 
-    static func generate() -> [String : AnyObject] {
-        let id = NSUUID().UUIDString
-        let title = NSUUID().UUIDString
+    static func generate() -> [String : Any] {
+        let id = UUID().uuidString
+        let title = UUID().uuidString
 
-        let dateFormatter = NSDateFormatter()
-        let enUSPosixLocale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let dateFormatter = DateFormatter()
+        let enUSPosixLocale = Locale(identifier: "en_US_POSIX")
         dateFormatter.locale = enUSPosixLocale
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-        let createdDate = dateFormatter.stringFromDate(NSDate())
+        let createdDate = dateFormatter.string(from: Date())
 
         return [
             "id" : id,
--- iOS/Source/AppDelegate.swift
+++ iOS/Source/AppDelegate.swift
@@ -13,8 +13,8 @@ class AppDelegate: UIResponder {
 }
 
 extension AppDelegate: UIApplicationDelegate {
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
         guard let window = self.window else { fatalError("Window not found") }
 
         let rootController = TableController(dataStack: self.dataStack)
